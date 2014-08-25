00b1941c1be1b62559d18bbfc59c17f692d6f9e9
csparpa <csparpa@gmail.com>
2014-08-26 00:49:47 +0100

    Add stubs for new modules and tests

---
+++ pyowm/webapi25/cityidregistry.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+"""
+Module containing a registry with lookup methods for OWM-provided city IDs
+"""
+
+class CityIDRegistry():
+
+    """
+    Initialise a registry that can be used
+
+    :param filepath_regex: Python format string that gives the path of the files
+           that store the city IDs information.
+           Eg: ``folder1/folder2/%02d-suffix.txt``
+    :type filepath_regex: str
+    :returns: a *CityIDRegistry* instance
+
+    """
+    def __init__(self, filepath_regex):
+        self._filepath_regex = filepath_regex
+
+    def id_for(self, city_name):
+        """
+        Returns the long ID corresponding to the provided city name.
+
+        :param city_name: the city name whose ID is looked up
+        :type city_name: str
+        :returns: a long or ``None`` if the lookup fails
+
+        """
+        pass
+
+    def location_for(self, city_name):
+        """
+        Returns the *Location* object corresponding to the provided city name.
+
+        :param city_name: the city name you want a *Location* for
+        :type city_name: str
+        :returns: a *Location* instance or ``None`` if the lookup fails
+
+        """
+        pass
+
---
+++ tests/functional/webapi25/test_cityidregistry.py
@@ -0,0 +1,12 @@
+"""
+Test case for cityidregistry.py module
+"""
+
+import unittest
+
+
+class TestCityIDRegistry(unittest.TestCase):
+
+    def test_template(self):
+        self.fail()
+
