aa348396474ebeebaeab66aaf401b3dfd8a44c10
Claudio Sparpaglione <claudio@walletsaver.com>
2016-05-30 17:23:21 +0200

    Messed up with Git... sunrise/sunset fix from #109 is OK now

--- tests/unit/webapi25/test_weather.py
+++ tests/unit/webapi25/test_weather.py
@@ -102,6 +102,32 @@ class TestWeather(unittest.TestCase):
               self.__test_visibility_distance, self.__test_dewpoint,
               self.__test_humidex, -10.0)
 
+    def test_init_stores_negative_sunset_time_as_none(self):
+        instance = Weather(self.__test_reference_time,
+                          -9876543210, self.__test_sunrise_time,
+                          self.__test_clouds,
+                          self.__test_rain, self.__test_snow, self.__test_wind,
+                          self.__test_humidity, self.__test_pressure,
+                          self.__test_temperature,
+                          self.__test_status, self.__test_detailed_status,
+                          self.__test_weather_code,
+                          self.__test_weather_icon_name,
+                          self.__test_visibility_distance, self.__test_dewpoint,
+                          self.__test_humidex, self.__test_heat_index)
+        self.assertIsNone(instance.get_sunset_time())
+
+    def test_init_stores_negative_sunrise_time_as_none(self):
+        instance = Weather(self.__test_reference_time,
+                          self.__test_sunset_time, -9876543210, self.__test_clouds,
+                          self.__test_rain, self.__test_snow, self.__test_wind,
+                          self.__test_humidity, self.__test_pressure,
+                          self.__test_temperature,
+                          self.__test_status, self.__test_detailed_status,
+                          self.__test_weather_code, self.__test_weather_icon_name,
+                          self.__test_visibility_distance, self.__test_dewpoint,
+                          self.__test_humidex, self.__test_heat_index)
+        self.assertIsNone(instance.get_sunrise_time())
+
     def test_from_dictionary(self):
         dict1 = {'clouds': {'all': 92}, 'name': 'London',
                  'coord': {'lat': 51.50853, 'lon': -0.12574},
