4210bd24f5c8696f513dde7460222585467ebd5b
csparpa <csparpa@gmail.com>
2017-05-11 19:29:10 +0200

    SO2Index class and test

--- tests/unit/webapi25/json_test_dumps.py
+++ tests/unit/webapi25/json_test_dumps.py
@@ -109,3 +109,13 @@ NO2INDEX_JSON_DUMP = '{"reference_time": 1234567, "no2_samples": [{"label": ' \
                     '-4.999999987376214e-07}], "location": {"country": "UK", ' \
                     '"name": "test", "coordinates": {"lat": 43.7, "lon": 12.3}, ' \
                     '"ID": 987}, "interval": "day", "reception_time": 1475283600}'
+
+SO2INDEX_JSON_DUMP = '{"reference_time": 1234567, "so2_samples": [{"pressure": ' \
+                    '1000, "value": 8.168363052618588e-08, "precision": ' \
+                    '-4.999999987376214e-07}, {"pressure": 681.2920532226562, ' \
+                    '"value": 8.686949115599418e-08, "precision": ' \
+                    '-4.999999987376214e-07}, {"pressure": 464.15887451171875, ' \
+                    '"value": 8.871462853221601e-08, "precision": ' \
+                    '-4.999999987376214e-07}], "location": {"country": "UK", ' \
+                    '"name": "test", "coordinates": {"lat": 43.7, "lon": 12.3}, ' \
+                    '"ID": 987}, "interval": "day", "reception_time": 1475283600}'
