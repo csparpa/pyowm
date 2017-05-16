9ad64d60a27260deae02f8f7651b9219db22b79d
csparpa <csparpa@gmail.com>
2017-05-16 21:50:31 +0200

    Fix bug: messed up file names and contents

--- tests/unit/webapi25/test_coindexparser.py
+++ tests/unit/webapi25/test_coindexparser.py
@@ -1,16 +1,16 @@
 import unittest
-from pyowm.webapi25.no2indexparser import NO2IndexParser
+from pyowm.webapi25.coindexparser import COIndexParser
 from pyowm.exceptions.parse_response_error import ParseResponseError
 from tests.unit.webapi25.json_test_responses import (
-    NO2INDEX_JSON, NO2INDEX_MALFORMED_JSON)
+    COINDEX_JSON, COINDEX_MALFORMED_JSON)
 
 
 class TestCOIndexParser(unittest.TestCase):
 
-    __instance = NO2IndexParser()
+    __instance = COIndexParser()
 
     def test_parse_JSON(self):
-        result = self.__instance.parse_JSON(NO2INDEX_JSON)
+        result = self.__instance.parse_JSON(COINDEX_JSON)
         self.assertIsNotNone(result)
         self.assertIsNotNone(result.get_reference_time())
         self.assertIsNotNone(result.get_reference_time())
@@ -21,8 +21,8 @@ class TestCOIndexParser(unittest.TestCase):
         self.assertIsNotNone(loc.get_lon())
         self.assertIsNotNone(loc.get_lat())
         self.assertIsNone(result.get_interval())
-        self.assertNotEquals(0, len(result.get_no2_samples()))
+        self.assertNotEquals(0, len(result.get_co_samples()))
 
     def test_parse_JSON_fails_with_malformed_JSON_data(self):
-        self.assertRaises(ParseResponseError, NO2IndexParser.parse_JSON,
-                          self.__instance, NO2INDEX_MALFORMED_JSON)
+        self.assertRaises(ParseResponseError, COIndexParser.parse_JSON,
+                          self.__instance, COINDEX_MALFORMED_JSON)
