eb13af3e3075f3122d256d697c5a1bf9422d59ce
Simone-Zabberoni <simone.zabberoni@gmail.com>
2017-12-12 00:09:00 +0100

    More refactoring due to codeclimate warnings

--- examples/getTemp.py
+++ examples/getTemp.py
@@ -3,29 +3,19 @@
 import pyowm
 from pyowm.exceptions import OWMError
 
-import sys, getopt
+import sys, argparse
 from datetime import datetime
 
-def usage():
-   print ('Usage: getTemp.py -k 60039jdfd09jf3a9 -p \"Some Place\"')
-   sys.exit(2)
+
 
 def main(argv):
-   api_key = ''
-   place = ''
-   try:
-      opts, args = getopt.getopt(argv, "k:p:",["key=","place="])
-   except getopt.GetoptError:
-      usage()
+   parser = argparse.ArgumentParser()
+   parser.add_argument('-k', required=True, metavar='your_api_key')
+   parser.add_argument('-p', required=True, metavar='\"Some Place,US\"')
+   args = parser.parse_args()
 
-   for opt, arg in opts:
-      if opt in ("-k", "--key"):
-         api_key = arg
-      elif opt in ("-p", "--place"):
-         place = arg
-
-   if (api_key == '' or place== ''):
-      usage()
+   api_key = args.k
+   place = args.p
 
    print ('Using key ' + api_key  + ' to query temperature in \"' + place  + '\"...' )   
    owm = pyowm.OWM(api_key) 
