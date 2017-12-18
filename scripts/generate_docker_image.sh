3a2fe84882e398c0fab8193200552de5ae664c14
csparpa <csparpa@gmail.com>
2017-12-18 20:37:20 +0100

    fix

--- scripts/generate_docker_image.sh
+++ scripts/generate_docker_image.sh
@@ -8,6 +8,6 @@ BASEIMAGENAME="csparpa/pyowm"
 cd ..
 for imagetype in $(find -maxdepth 1 -name 'Dockerfile.*' -printf '%P\n' | cut -d'.' -f2); do
 	echo "Building from Dockerfile.$imagetype to image $BASEIMAGENAME:${VERSION}.$imagetype ..."
-	docker build . -f dockerfiles/Dockerfile.$imagetype -t $BASEIMAGENAME:${VERSION}.$imagetype
+	docker build -f dockerfiles/Dockerfile.$imagetype -t $BASEIMAGENAME:${VERSION}.$imagetype .
 done
 echo "done"
