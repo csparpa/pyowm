589ad77e1b5cd2bf8382d5c6aac9695d111de8a3
csparpa <csparpa@gmail.com>
2018-04-14 10:30:40 +0200

    small fixes

--- tests/integration/commons/test_http_client.py
+++ tests/integration/commons/test_http_client.py
@@ -10,21 +10,21 @@ class TestHTTPClient(unittest.TestCase):
     instance = HttpClient()
 
     def test_get_json_against_httpbin_ok(self):
-        # https://httpbin.org/ip
+        # http://httpbin.org/ip
         status, data = self.instance.get_json('http://httpbin.org/ip')
         self.assertEqual(200, status)
         self.assertIsInstance(data, dict)
 
     def test_get_json_against_httpbin_status_code_ko(self):
-        # https://httpbin.org/status/400
+        # http://httpbin.org/status/400
         expected_status = 400
 
         self.assertRaises(api_call_error.APICallError, HttpClient.get_json,
-                          self.instance, 'https://httpbin.org/status/' +
+                          self.instance, 'http://httpbin.org/status/' +
                                               str(expected_status))
 
     def test_get_json_against_httpbin_parse_error(self):
-        # https://httpbin.org/xml
+        # http://httpbin.org/xml
         try:
             status, data = self.instance.get_json('http://httpbin.org/xml')
             self.fail()
@@ -32,20 +32,21 @@ class TestHTTPClient(unittest.TestCase):
             pass
 
     def test_put_against_httpbin(self):
-        # https://httpbin.org/put
+        # http://httpbin.org/put
         formdata = dict(a=1, b=2, c=3)
         status, data = self.instance.put('http://httpbin.org/put', data=formdata)
         self.assertEqual(200, status)
         self.assertIsInstance(data, dict)
-        self.assertEquals(formdata, data['json'])
+        self.assertEqual(formdata, data['json'])
 
     def test_delete_against_httpbin(self):
-        # https://httpbin.org/delete
+        # http://httpbin.org/delete
         formdata = dict(a=1, b=2, c=3)
         status, data = self.instance.delete('http://httpbin.org/delete', data=formdata)
         self.assertEqual(200, status)
         self.assertIsInstance(data, dict)
         self.assertEqual(formdata, data['json'])
+
 
 if __name__ == "__main__":
     unittest.main()
