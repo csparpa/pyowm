3920a826755b6697f24dd14aa17eabff51227611
csparpa <csparpa@gmail.com>
2018-07-18 21:01:01 +0200

    Split installation test and dependencies test on: local install + remote (Pypi) install

--- tests/local_installation_test.sh
+++ tests/local_installation_test.sh
@@ -1,16 +1,23 @@
 #!/usr/bin/env bash
-echo " --- Start of installation test ..."
+echo " --- Start of local installation test ..."
 venv="$(readlink -f $(date +%s)_pyowm_installation_test)"
 virtualenv "$venv" && cd "$venv" && source bin/activate
 echo " --- Created virtualenv: $venv ..."
 
-# Test PyOWM installation
-pip install pyowm
+# Understand if running on localhost or on Travis CI
+if [ -z "$TRAVIS_BUILD_DIR" ]; then
+   TRAVIS_BUILD_DIR="$(readlink -f ../..)"
+fi
+echo "PyOWM source package is in: $TRAVIS_BUILD_DIR"
+
+# Test PyOWM installation from local folder
+cd "$TRAVIS_BUILD_DIR"
+pip install -e .
 if [ $? -ne 0 ]; then
     echo " --- Pip installation failed!"
     exit 1
 fi
-echo " --- Pip installation was OK ..."
+echo " --- Pip installation from local folder was OK ..."
 python -c "import pyowm"
 if [ $? -ne 0 ]; then
     echo " --- Test import of library failed!"
---
+++ tests/pypi_installation_test.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+echo " --- Start of Pypi installation test ..."
+venv="$(readlink -f $(date +%s)_pyowm_installation_test)"
+virtualenv "$venv" && cd "$venv" && source bin/activate
+echo " --- Created virtualenv: $venv ..."
+
+# Test
+pip install pyowm
+if [ $? -ne 0 ]; then
+    echo " --- Pip installation failed!"
+    exit 1
+fi
+echo " --- Pip installation was OK ..."
+python -c "import pyowm"
+if [ $? -ne 0 ]; then
+    echo " --- Test import of library failed!"
+    exit 2
+fi
+echo " --- Test import of library was OK"
+
+# Cleanup
+deactivate
+rm -rf "$venv"
+exit 0
