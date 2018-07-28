70f642c13baf7ce1e38ac700f3ac280ff5c175c7
csparpa <csparpa@gmail.com>
2018-07-28 10:04:52 +0200

    Split UV Index API integration tests

--- tests/integration/webapi25/test_integration_webapi25.py
+++ tests/integration/webapi25/test_integration_webapi25.py
@@ -539,41 +539,6 @@ class IntegrationTestsWebAPI25(unittest.TestCase):
             weat = item.get_weather()
             self.assertTrue(weat is not None)
 
-    def test_uvindex_around_coords(self):
-        """
-        Test feature: get UV index around geo-coordinates.
-        """
-        u = self.__owm.uvindex_around_coords(45, 9)
-        self.assertIsNotNone(u)
-        self.assertIsNotNone(u.get_value())
-        self.assertIsNotNone(u.get_reception_time())
-        self.assertIsNotNone(u.get_location())
-
-    def test_uvindex_forecast_around_coords(self):
-        """
-        Test feature: get UV index forecast around geo-coordinates.
-        """
-        uv_list = self.__owm.uvindex_forecast_around_coords(45, 9)
-        self.assertIsInstance(uv_list, list)
-        for item in uv_list:
-            self.assertIsNotNone(item.get_value())
-            self.assertIsNotNone(item.get_reception_time())
-            self.assertIsNotNone(item.get_location())
-
-    def test_uvindex_history_around_coords(self):
-        """
-        Test feature: get UV index history around geo-coordinates.
-        """
-        start = datetime(2017, 6, 21)
-        end = datetime(2017, 6, 27)
-        uv_list = self.__owm.uvindex_history_around_coords(37.7, -122.37,
-                                                           start,
-                                                           end=end)
-        self.assertIsInstance(uv_list, list)
-        for item in uv_list:
-            self.assertIsNotNone(item.get_value())
-            self.assertIsNotNone(item.get_reception_time())
-            self.assertIsNotNone(item.get_location())
 
     def test_coindex_around_coords(self):
         """
