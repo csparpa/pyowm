d960a15384cd678633fdd3142491211e1b00aae6
csparpa <csparpa@gmail.com>
2018-10-30 22:37:44 +0100

    test 5

--- deploy/deploy_to_pypi.sh
+++ deploy/deploy_to_pypi.sh
@@ -4,6 +4,7 @@
 #  - deploy to Test PyPI upon every push
 
 if [ $TRAVIS_BRANCH = "develop" ] && [[ $TRAVIS_EVENT_TYPE == "push" ]]; then
+    echo '*** Will build the DEVELOP branch and publish to Test PyPI ( $TEST_PYPI_URL )'
     export INDEX_URL="$TEST_PYPI_URL"
     export PYPI_USERNAME="$TEST_PYPI_USERNAME"
     export PYPI_PASSWORD="$TEST_PYPI_PASSWORD"
@@ -12,19 +13,19 @@ else
     exit 5
 fi
 
-echo 'Generating source distribution...'
+echo '*** Generating source distribution...'
 python setup.py sdist
 
-echo 'Generating egg distribution...'
+echo '*** Generating egg distribution...'
 python setup.py bdist_egg
 
-echo 'Generating wheel distribution...'
+echo '*** Generating wheel distribution...'
 python setup.py bdist_wheel
 
-echo 'Uploading all artifacts to PyPi...'
+echo '*** Uploading all artifacts to PyPi...'
 twine upload --repository-url "$INDEX_URL" \
              --username "$PYPI_USERNAME" \
              --password "$PYPI_PASSWORD" \
              --skip-existing \
              dist/*
-echo 'Done'
+echo '*** Done'
---
+++ tests/run_unit_tests.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+set -o errexit
+
+cd ..
+tox
