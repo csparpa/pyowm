2ad9a23ea6e09e88cce5c426b4a171f9807eab74
csparpa <csparpa@gmail.com>
2019-04-30 19:00:58 +0200

    forgot to commit...

---
+++ pyowm/__version__.py
@@ -0,0 +1,11 @@
+from pyowm import constants
+from pyowm.utils.strings import version_tuple_to_str
+
+__title__ = 'pyowm'
+__description__ = 'A Python wrapper around OpenWeatherMap web APIs'
+__url__ = 'https://github.com/csparpa/pyowm'
+__version__ = version_tuple_to_str(constants.PYOWM_VERSION)
+__author__ = 'Claudio Sparpaglione'
+__author_email__ = 'csparpa@gmail.com'
+__author_twitter__ = '@csparpa'
+__license__ = 'MIT'
