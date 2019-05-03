8a55274e84014444add1d4d23760a1ce2c1bd38a
csparpa <csparpa@gmail.com>
2019-05-03 23:06:09 +0200

    Fix error and introduce find_packages()

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+from setuptools import setup, find_packages
 from pyowm import constants
 from pyowm.utils.strings import version_tuple_to_str
 from pyowm.__version__ import __author__, __author_email__, __description__, __license__, __title__,\
@@ -13,18 +13,7 @@ setup(
     author=__author__,
     author_email=__author_email__,
     url=__url__,
-    packages=['pyowm',
-              'pyowm.abstractions',
-              'pyowm.agroapi10',
-              'pyowm.alertapi30',
-              'pyowm.commons',
-              'pyowm.exceptions',
-              'pyowm.pollutionapi30', 'pyowm.pollutionapi30.xsd',
-              'pyowm.uvindexapi30', 'pyowm.uvindexapi30.xsd',
-              'pyowm.tiles',
-              'pyowm.utils',
-              'pyowm.weatherapi25', 'pyowm.weatherapi25.cityids', 'pyowm.weatherapi25.xsd',
-              'pyowm.stationsapi30', 'pyowm.stationsapi30.xsd'],
+    packages=find_packages(),
     long_description="""PyOWM is a client Python wrapper library for OpenWeatherMap web APIs. It allows quick and easy 
     consumption of OWM data from Python applications via a simple object model and in a human-friendly fashion.""",
     include_package_data=True,
@@ -32,7 +21,7 @@ setup(
         'requests>=2.20.0,<3',
         'geojson>=2.3.0,<3'
     ],
-    python_requires='>=3.4',
+    python_requires='>=3.7',
     classifiers=[
       "License :: OSI Approved :: MIT License",
       "Programming Language :: Python",
