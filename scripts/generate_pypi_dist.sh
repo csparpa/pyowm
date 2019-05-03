f1477362554b89a3121d1d89baa31fdf05c781e7
csparpa <csparpa@gmail.com>
2019-05-03 23:56:34 +0200

    fix for Py38

--- scripts/generate_pypi_dist.sh
+++ scripts/generate_pypi_dist.sh
@@ -6,20 +6,16 @@ cd ..
 rm -fv build/*
 rm -fv dist/*
 echo 'Generating source distribution...'
-python2.7 setup.py sdist
+python3.7 setup.py sdist
 
 echo 'Generating .egg distributions...'
-python3.4 setup.py bdist_egg
-python3.5 setup.py bdist_egg
-python3.6 setup.py bdist_egg
 python3.7 setup.py bdist_egg
+python3.8 setup.py bdist_egg
 
 echo 'Generating Wheel distributions...'
-python2 setup.py sdist bdist_wheel
 python3 setup.py sdist bdist_wheel
 
 echo 'Generating Windows self-extracting .exe ...'
-python2 setup.py bdist_wininst
 python3 setup.py bdist_wininst
 
 
