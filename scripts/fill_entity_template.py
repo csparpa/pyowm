6b40638a91b30c6d946ee48d1e83222bf118f8d6
csparpa <csparpa@gmail.com>
2019-05-04 00:31:36 +0200

    Generating new entity classes with a script

---
+++ scripts/fill_entity_template.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+if __name__ == '__main__':
+
+    import sys
+    from jinja2 import Template
+
+    param_1 = sys.argv[1]
+    entity_name = param_1.title()
+
+    try:
+        param_2 = sys.argv[2]
+        if param_2 == '--iterable':
+            is_iterable = True
+        else:
+            is_iterable = False
+    except:
+        is_iterable = False
+
+    with open('new_entity.template.txt') as i:
+        tpl = Template(i.read())
+        rendered_text = tpl.render(entity_name=entity_name, is_iterable=is_iterable)
+        with open('{}.py'.format(entity_name.lower()), 'w') as o:
+            o.write(rendered_text)
