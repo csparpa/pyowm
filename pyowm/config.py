07864717db465c70973f196fb0d4024e3d3d9270
csparpa <csparpa@gmail.com>
2019-05-14 20:22:12 +0200

    remove API key from config and increase timeout

--- pyowm/config.py
+++ pyowm/config.py
@@ -2,12 +2,11 @@
 # -*- coding: utf-8 -*-
 
 DEFAULT_CONFIG = {
-    'api_key': 'b1b15e88fa797225412429c1c50c122a',   # fake API key, won't work
     'subscription_type': 'free',
     'language': 'en',
     'connection': {
         'use_ssl': False,
         'verify_ssl_certs': True,
-        'timeout_secs': 2
+        'timeout_secs': 5
     }
 }
