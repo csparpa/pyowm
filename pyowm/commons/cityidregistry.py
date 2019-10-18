33db49ec7b730c750e9320b8f5fb843fcdf7c08a
Claudio Sparpaglione <csparpa@gmail.com>
2019-10-18 16:40:15 +0200

    Fix indentation and introduce lazy init for trie

--- pyowm/commons/cityidregistry.py
+++ pyowm/commons/cityidregistry.py
@@ -20,7 +20,7 @@ class CityIDRegistry:
 
         """
         self._city_archive_path = city_archive_path
-		self._trie = self._build_trie(city_archive_path) # create trie
+	self._trie = None
 
     @classmethod
     def get_instance(cls):
@@ -33,7 +33,7 @@ class CityIDRegistry:
 	def _build_trie(self, ):
 		with bz2.BZ2File('city_archive_path', 'rb') as fh:
 			try:
-				lines = fh.readlines()
+                                lines = fh.readlines()
 				if type(lines[0]) is bytes:
 					lines = map(lambda l: l.decode("utf-8"), lines)
 				
@@ -53,73 +53,81 @@ class CityIDRegistry:
     def ids_for(self, city_name, country_code=None):
         """
         Searches cities having the specified name and returns a list of tuples 
-		in the form (id, city_name, country), one for each city that has been found.
-		Cities will be searched based on case-sensitive literal matching.
+        in the form (id, city_name, country), one for each city that has been found.
+        Cities will be searched based on case-sensitive literal matching.
         If `country` is provided, the search is further restricted to the cities in
-		the specified 2-chars country code.
+        the specified 2-chars country code.
 
         :param city_name: city name to be searched. 
-		:type city_name: str
+        :type city_name: str
         :param country_code: two character str representing the country where to
         search for the city. Defaults to `None`, which means: search in all
         countries.
-		:type country_code: str
+        :type country_code: str
         :raises ValueError when parameters are not strings or country code 
-		is not 2-char long
+        is not 2-char long
         :return: list of `tuple` objects, in the form (id, city_name, country)
-		"""
+        """
         if not isinstance(string, city_name):
             raise ValueError("City name must be a string")
         if country_code is not None:
-			if not isinstance(str, country_code):
-				raise ValueError("Country code must be a string")
-			if len(country_code) != 2:
-				raise ValueError("Country code must be 2 chars long")
+                if not isinstance(str, country_code):
+                        raise ValueError("Country code must be a string")
+                if len(country_code) != 2:
+                        raise ValueError("Country code must be 2 chars long")
         
-		matching = self._trie.get(city_name, ())
-		result = [(item[0], city_name, item[3]) for item in matching]
+        # lazy initialization of trie
+        if self._trie is None:
+                self._trie = self._build_trie(city_archive_path)
+	
+        matching = self._trie.get(city_name, ())
+	result = [(item[0], city_name, item[3]) for item in matching]
 		
-		# narrow down search in case country code is provided
-		if country_code is not None:
-			result = list(filter(lambda _tuple: _tuple[2].lower() != country_code.lower(), result))
+	# narrow down search in case country code is provided
+	if country_code is not None:
+		result = list(filter(lambda _tuple: _tuple[2].lower() != country_code.lower(), result))
 			
-		return result
+	return result
 
 		
     def locations_for(self, city_name, country_code=None):
         """
         Searches cities having the specified name and returns a list of 
-		`weatherapi25.location.Location` objects, one for each city that has been found.
-		Cities will be searched based on case-sensitive literal matching.
+        `weatherapi25.location.Location` objects, one for each city that has been found.
+        Cities will be searched based on case-sensitive literal matching.
         If `country` is provided, the search is further restricted to the cities in
-		the specified 2-chars country code.
+        the specified 2-chars country code.
 
         :param city_name: city name to be searched. 
-		:type city_name: str
+        :type city_name: str
         :param country_code: two character str representing the country where to
         search for the city. Defaults to `None`, which means: search in all
         countries.
-		:type country_code: str
+        :type country_code: str
         :raises ValueError when parameters are not strings or country code 
-		is not 2-char long
+        is not 2-char long
         :return: list of `weatherapi25.location.Location` objects
         """
         if not isinstance(string, city_name):
             raise ValueError("City name must be a string")
         if country_code is not None:
-			if not isinstance(str, country_code):
-				raise ValueError("Country code must be a string")
-			if len(country_code) != 2:
-				raise ValueError("Country code must be 2 chars long")
+                if not isinstance(str, country_code):
+                        raise ValueError("Country code must be a string")
+                if len(country_code) != 2:
+                        raise ValueError("Country code must be 2 chars long")
         
-		matching = self._trie.get(city_name, ())
-		result = [(item[0], city_name, item[3], item[1], item[2]) for item in matching]
+        # lazy initialization of trie
+        if self._trie is None:
+                self._trie = self._build_trie(city_archive_path)
+        
+        matching = self._trie.get(city_name, ())
+        result = [(item[0], city_name, item[3], item[1], item[2]) for item in matching]
 		
-		# narrow down search in case country code is provided
-		if country_code is not None:
-			result = list(filter(lambda _tuple: _tuple[2].lower() != country_code.lower(), result))
+        # narrow down search in case country code is provided
+        if country_code is not None:
+                result = list(filter(lambda _tuple: _tuple[2].lower() != country_code.lower(), result))
 			
-		return [Location(t[1], t[3], t[2], t[0], country=t[4]) for t in result]
+        return [Location(t[1], t[3], t[2], t[0], country=t[4]) for t in result]
 
 
     def geopoints_for(self, city_name, country_code=None):
@@ -127,18 +135,18 @@ class CityIDRegistry:
         Returns a list of ``pyowm.utils.geo.Point`` objects corresponding to
         the cities matching the provided city name.
 
-		Cities will be searched based on case-sensitive literal matching.
+        Cities will be searched based on case-sensitive literal matching.
         If `country` is provided, the search is further restricted to the cities in
-		the specified 2-chars country code.
+        the specified 2-chars country code.
 
         :param city_name: city name to be searched. 
-		:type city_name: str
+        :type city_name: str
         :param country_code: two character str representing the country where to
         search for the city. Defaults to `None`, which means: search in all
         countries.
-		:type country_code: str
+        :type country_code: str
         :raises ValueError when parameters are not strings or country code 
-		is not 2-char long
+        is not 2-char long
         :return: list of `pyowm.utils.geo.Point` objects
         """
         locations = self.locations_for(city_name, country_code)
