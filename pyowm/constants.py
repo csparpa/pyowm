e7ccb5ec3f86bee0cbb9981070a62988a83a5b3c
csparpa <csparpa@gmail.com>
2020-10-02 18:31:53 +0200

    forgot... :-((((

--- pyowm/constants.py
+++ pyowm/constants.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-PYOWM_VERSION = (3, 1, 0)
+PYOWM_VERSION = (3, 1, 1)
 AGRO_API_VERSION = (1, 0, 0)
 AIRPOLLUTION_API_VERSION = (3, 0, 0)
 ALERT_API_VERSION = (3, 0, 0)
